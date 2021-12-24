69dfc854bbf61d143a326adcfc9914d0284b5e03
Richard2415 <richardselvaraj07@gmail.com>
2021-12-24 22:11:38 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,32 @@
+module.exports = {
+    1: {
+        name: 'Richard',
+        age: 25,
+        occupation: 'Full Stack Developer'
+    },
+    2: {
+        name: 'Danish',
+        age: 28,
+        occupation: 'ICF'
+    },
+    3: {
+        name: 'Jack',
+        age: 20,
+        occupation: 'Engineer'
+    },
+    4: {
+        name: 'Leenora',
+        age: 20,
+        occupation: 'Bank Employee'
+    },
+    5: {
+        name: 'Sam',
+        age: 19,
+        occupation: 'Data Scientist'
+    },
+    6: {
+        name: 'Mark',
+        age: 50,
+        occupation: 'Chief Technical Officer'
+    }
+}
